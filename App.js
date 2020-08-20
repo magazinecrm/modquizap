5cca73cb241ca23a02c74219d5e51ff7c633cf47
pjohny <34673267+magazinecrm@users.noreply.github.com>
2020-08-20 15:18:44 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Facebook from './components/Facebook';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Facebook Authentication
+        </p>
+        <Facebook />
+    </div>
+  );
+}
