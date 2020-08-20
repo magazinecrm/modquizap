80789c2657578b5a2d57160f0270fb9f18047a5d
pjohny <34673267+magazinecrm@users.noreply.github.com>
2020-08-20 15:21:14 +0530

    Create Facebook.js

---
+++ Facebook.js
@@ -0,0 +1,57 @@
+[ec2-user@swisshealthangelmagazine components]$ cat Facebook.js
+import React, {Component} from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+export default class Facebook extends Component {
+
+    state = {
+        auth: false,
+        name: '',
+        picture: ''
+    };
+
+    responseFacebook = response => {
+        console.log(response);
+        if(response.status !== 'unknown')
+        this.setState({
+            auth: true,
+            name: response.name,
+            picture: response.picture.data.url
+        });
+
+    }
+
+    componentClicked = () => {
+        console.log('Facebook btn clicked');
+    }
+
+    render(){
+        let facebookData;
+
+        this.state.auth ?
+        facebookData = (
+                <div style={{
+                    width: '400px',
+                    margin: 'auto',
+                    background: '#f4f4f4',
+                    padding: '20px',
+                    color: '#000'
+                }}>
+                    <img src={this.state.picture} alt={this.state.name} />
+                    <h2>Welcome {this.state.name}! Do you know God is great</h2>
+                </div>
+            ) :
+            facebookData = (<FacebookLogin
+                appId="347072892755118"
+                autoLoad={true}
+                fields="name,picture"
+                onClick={this.componentClicked}
+                callback={this.responseFacebook} />);
+
+        return (
+            <>
+                {facebookData}
+            </>
+        );
+    }
+}
